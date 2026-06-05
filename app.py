4d47dc465042bed02d0b9aa964bb636bac82736e
GPDB-F1 <isaac.j.colclough@gmail.com>
2026-06-05 10:16:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ start_address = st.text_input("Start address", "Cambridge Circus, London")
 end_address = st.text_input("End address", "Oxford Circus, London")
 datetime_str = st.text_input("Date and time (YYYY-MM-DD HH:MM)", "2025-07-14 14:00")
 
-if st.button("Find Shaded Route")
+if st.button("Find Shaded Route"):
     st.write("Calculating...")
     start_lat, start_lon = get_coordinates(start_address)
     end_lat, end_lon = get_coordinates(end_address)
