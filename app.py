52736795d7a5a4c522bff94cf09901010f7b4de6
GPDB-F1 <isaac.j.colclough@gmail.com>
2026-09-03 14:57:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #Import packages from library
 import streamlit as st
 from streamlit_folium import st_folium
-from routing import shaded_route, get_coordinates
+from routing import shaded_route, get_coordinates, monte_carlo_eta
 
 #Streamlit interface
 st.title("Shade-Aware Routing Algorithm")
@@ -12,21 +12,21 @@ end_address = st.text_input("End address", "Oxford Circus, London")
 datetime_str = st.text_input("Date and time (YYYY-MM-DD HH:MM)", "2025-07-14 14:00")
 
 if st.button("Find Shaded Route"):
-  st.write("Calculating...")
   start_lat, start_lon = get_coordinates(start_address)
   end_lat, end_lon = get_coordinates(end_address)
   if start_lat is None or end_lat is None:
     st.error("One or both addresses could not be found. Please try again.")
   else:
-    m, G, shaded_path, fastest_path = shaded_route(start_lat, start_lon, end_lat, end_lon, datetime_str)
-    if shaded_path and fastest_path:
-      shaded_eta = monte_carlo_eta(G, shaded_path, datetime_str)
-      fastest_eta = monte_carlo_eta(G, fastest_path, datetime_str)
-      st.session_state["map"] = m
-      st.session_state["shaded_eta"] = shaded_eta
-      st.session_state["fastest_eta"] = fastest_eta
-    else:
-      st.session_state["map"] = m
+    with st.spinner("Calculating..."):
+      m, G, shaded_path, fastest_path = shaded_route(start_lat, start_lon, end_lat, end_lon, datetime_str)
+      if shaded_path and fastest_path:
+        shaded_eta = monte_carlo_eta(G, shaded_path, datetime_str)
+        fastest_eta = monte_carlo_eta(G, fastest_path, datetime_str)
+        st.session_state["map"] = m
+        st.session_state["shaded_eta"] = shaded_eta
+        st.session_state["fastest_eta"] = fastest_eta
+      else:
+        st.session_state["map"] = m
 
 if "map" in st.session_state:
   if "shaded_eta" in st.session_state:
