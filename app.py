36bc4e403861ea7f2ef4f836713aa5b21136b0fe
GPDB-F1 <isaac.j.colclough@gmail.com>
2026-09-04 15:07:35 +0100

    Update app.py
    datetime_str takes the current time rather than inputted

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.write("Finds a walking route to keep users in a variable quantity of shade")
 
 start_address = st.text_input("Start address", "Cambridge Circus, London")
 end_address = st.text_input("End address", "Oxford Circus, London")
-datetime_str = st.text_input("Date and time (YYYY-MM-DD HH:MM)", "2025-07-14 14:00")
+datetime_str = st.text_input("Date and time (YYYY-MM-DD HH:MM)", pd.Timestamp.now().strftime("%Y-%m-%d %H:%M"))
 
 if st.button("Find Shaded Route"):
   start_lat, start_lon = get_coordinates(start_address)
